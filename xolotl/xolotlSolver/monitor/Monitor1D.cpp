cff8e8c801c0e7fec43a82df8f0b319683e2f788
Sophie Blondel <sophie.blondel1@gmail.com>
2021-02-03 07:31:19 -0800

    Fixed the additional bursting criteria.

--- xolotl/xolotlSolver/monitor/Monitor1D.cpp
+++ xolotl/xolotlSolver/monitor/Monitor1D.cpp
@@ -2469,7 +2469,7 @@ PetscErrorCode eventFunction1D(TS ts, PetscReal time, Vec solution,
 						+ cbrt((3.0 * tlcCubed * nV) / (8.0 * xolotlCore::pi))
 						- cbrt((3.0 * tlcCubed) / (8.0 * xolotlCore::pi));
 
-				if ((distance - radius) / distance < 0.5) continue;
+				if (radius / distance < 0.5) continue;
 
 				// Add randomness
 				double prob = prefactor * (1.0 - (distance - radius) / distance)
